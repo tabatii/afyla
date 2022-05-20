11c3a7be8c25232890413078f9f3d298330b2af2
tabatii <anatanjaawi@gmail.com>
2022-05-20 21:07:32 +0100

    add order to roles table

--- database/migrations/2022_05_20_200126_add_order_to_roles_table.php
+++ database/migrations/2022_05_20_200126_add_order_to_roles_table.php
@@ -14,7 +14,8 @@ return new class extends Migration
     public function up()
     {
         Schema::table('roles', function (Blueprint $table) {
-            $table->integer('order')->after('display_name');
+            //$table->integer('order')->after('display_name');
+            $table->dropColumn('order');
         });
     }
 
